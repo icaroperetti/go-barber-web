7d4596f9f217ef9765c72095f15f03e54b6de39a
Icaro Peretti <icaroperetti50@gmail.com>
2022-01-30 16:18:44 -0300

    feat:Creating Toast component

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,14 @@ import GlobalStyle from "./styles/global";
 import SignIn from "./pages/SignIn";
 import { AuthProvider } from "./hooks/AuthContext";
 // import SignUp from "./pages/SignUp";
-
+import ToastContainer from "./components/ToastContainer";
 const App: React.FC = () => {
   return (
     <>
       <AuthProvider>
         <SignIn />
       </AuthProvider>
+      <ToastContainer />
       <GlobalStyle />
     </>
   );
--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -35,8 +35,12 @@ const SignIn: React.FC = () => {
           password: data.password,
         });
       } catch (err: any) {
-        const errors = getValidationErrors(err);
-        formRef.current?.setErrors(errors);
+        if (err instanceof Yup.ValidationError) {
+          const errors = getValidationErrors(err);
+          formRef.current?.setErrors(errors);
+        }
+
+        //Toast Message
       }
     },
     [signIn]
