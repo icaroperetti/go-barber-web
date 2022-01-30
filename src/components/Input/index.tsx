74b17621fcc0d6fa7ccde6c40e1de6e6a46e9b57
Icaro Peretti <icaroperetti50@gmail.com>
2022-01-30 13:00:41 -0300

    feat: Adding some styles to input

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,29 +1,53 @@
-import React,{InputHTMLAttributes,useEffect,useRef} from 'react'
-import {IconBaseProps} from 'react-icons'
-import { Container } from './styles';
-import { useField} from '@unform/core'
-interface InputProps extends InputHTMLAttributes<HTMLInputElement>{
+import React, {
+  InputHTMLAttributes,
+  useEffect,
+  useRef,
+  useState,
+  useCallback,
+} from "react";
+import { IconBaseProps } from "react-icons";
+import { Container } from "./styles";
+import { useField } from "@unform/core";
+
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   name: string;
   icon: React.ComponentType<IconBaseProps>;
 }
-const Input: React.FC<InputProps> = ({name,icon:Icon,...rest}) => {
-  const inputRef = useRef(null);
-  const {fieldName,defaultValue,error,registerField} = useField(name)
+const Input: React.FC<InputProps> = ({ name, icon: Icon, ...rest }) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+  const [isFocused, setIsFocused] = useState(false);
+  const [isFilled, setIsFilled] = useState(false);
+  const { fieldName, defaultValue, error, registerField } = useField(name);
 
-  useEffect(()=>{
+  const handleInputBlur = useCallback(() => {
+    setIsFocused(false);
+    setIsFilled(!!inputRef.current?.value);
+  }, []);
+
+  const handleInputFocus = useCallback(() => {
+    setIsFocused(true);
+  }, []);
+
+  useEffect(() => {
     registerField({
-      name:fieldName,
-      ref:inputRef.current,
-      path:'value'
-    })
-  },[fieldName,registerField])
+      name: fieldName,
+      ref: inputRef.current,
+      path: "value",
+    });
+  }, [fieldName, registerField]);
 
-  return(
-    <Container>
-    {Icon && <Icon size={20} />}
-    <input defaultValue={defaultValue} ref={inputRef} {...rest} />
+  return (
+    <Container isFocused={isFocused} isFilled={isFilled}>
+      {Icon && <Icon size={20} />}
+      <input
+        defaultValue={defaultValue}
+        ref={inputRef}
+        onFocus={handleInputFocus}
+        onBlur={handleInputBlur}
+        {...rest}
+      />
     </Container>
-  )
-}
+  );
+};
 
 export default Input;
--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -1,30 +1,49 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
-export const Container = styled.div`
-  color: #f4ede8;
+interface ContainerProps {
+  isFocused: boolean;
+  isFilled: boolean;
+}
+export const Container = styled.div<ContainerProps>`
   background: #232129;
   border-radius: 10px;
-  border: 2px solid #232129;
   padding: 16px;
   width: 100%;
   display: flex;
   align-items: center;
 
+  border: 2px solid #232129;
+  color: #666360;
+
   & + div {
     margin-top: 16px;
   }
+
+  ${(props) =>
+    props.isFocused &&
+    css`
+      color: #ff9000;
+      border-color: #ff9000;
+      background: transparent;
+    `}
+
+  ${(props) =>
+    props.isFilled &&
+    css`
+      color: #ff9000;
+    `}
 
   input {
     color: white;
     background: transparent;
     flex: 1;
     border: 0;
+    width: 100%;
     &::placeholder {
       color: #666360;
     }
   }
   svg {
     margin-right: 16px;
-    color: #666360;
   }
 `;
