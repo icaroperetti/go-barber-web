0988224f2a43af93c203eae75eed68f1646f4857
Icaro Peretti <icaroperetti50@gmail.com>
2022-01-30 15:35:15 -0300

    feat:Creating hook for useAuth

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useCallback, useState } from "react";
+import React, { createContext, useCallback, useState, useContext } from "react";
 import api from "../services/api";
 
 interface SignInCredentials {
@@ -16,9 +16,17 @@ interface AuthState {
   user: object;
 }
 
-export const AuthContext = createContext({} as AuthContextData);
+const AuthContext = createContext({} as AuthContextData);
 
-export const AuthProvider: React.FC = ({ children }) => {
+const useAuth = (): AuthContextData => {
+  const context = useContext(AuthContext);
+  if (!context) {
+    throw new Error("useAuth must be used within an AuthProvider");
+  }
+  return context;
+};
+
+const AuthProvider: React.FC = ({ children }) => {
   const [data, setData] = useState<AuthState>(() => {
     const token = localStorage.getItem("@GoBarber:token");
     const user = localStorage.getItem("@GoBarber:user");
@@ -28,6 +36,7 @@ export const AuthProvider: React.FC = ({ children }) => {
     }
     return {} as AuthState;
   });
+
   const signIn = useCallback(async ({ email, password }) => {
     const response = await api.post("/sessions", {
       email,
@@ -47,3 +56,5 @@ export const AuthProvider: React.FC = ({ children }) => {
     </AuthContext.Provider>
   );
 };
+
+export { AuthProvider, useAuth };
