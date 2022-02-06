6d7fc88b587836cc481173ed7f0d571c88739b75
Icaro Peretti <icaroperetti50@gmail.com>
2022-02-06 00:18:35 -0300

    Creating animation for Toast message

--- src/components/ToastContainer/Toast/styles.ts
+++ src/components/ToastContainer/Toast/styles.ts
@@ -1,5 +1,5 @@
 import styled, { css } from "styled-components";
-
+import { animated } from "react-spring";
 interface ToastProps {
   type?: "success" | "error" | "info";
   hasDescription: boolean;
@@ -20,7 +20,7 @@ const toastTypeVariations = {
   `,
 };
 
-export const Container = styled.div<ToastProps>`
+export const Container = styled(animated.div)<ToastProps>`
   width: 360px;
   position: relative;
   padding: 16px 30px 16px 16px;
